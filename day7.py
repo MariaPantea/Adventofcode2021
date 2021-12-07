743d60a652319206d37fb9d10dca7e030392b2a4
Maria Larsson <maria@sleepcycle.com>
2021-12-07 17:04:39 +0100

    Update day 7

--- day7.py
+++ day7.py
@@ -1,35 +1,30 @@
+import statistics
+
 from utils import read_input_as_one_line_ints
-from collections import Counter
 
 
-def get_fuel(x, y, is_part1):
-    if is_part1:
-        return abs(x - y)
-    else:
-        n = abs(x - y)
-        return n*(n+1)//2
+def part_1():
+    # The median of crab positions is the optimal position
+    median = int(statistics.median(data))
+    fuel = 0
+    for pos in data:
+        fuel += abs(pos - median)
+
+    return fuel
 
 
-def get_best_fuel_consumption(is_part1):
-    min_fuel_cells = 100000000
+def part_2():
+    # The mean of crab positions is the optimal position
+    avg = int(statistics.mean(data))
+    fuel = 0
+    for pos in data:
+        n = abs(pos - avg)
+        fuel += n * (n + 1) // 2
 
-    for p in range(min_position, max_position):
-        fuel_cells = 0
-        for i in crabs.keys():
-            fuel = get_fuel(i, p, is_part1)
-            fuel_cells += crabs[i] * fuel
-
-        if fuel_cells < min_fuel_cells:
-            min_fuel_cells = fuel_cells
-
-    return min_fuel_cells
+    return fuel
 
 
 if __name__ == '__main__':
     data = read_input_as_one_line_ints('inputs/day7.txt')
-    max_position = max(data)
-    min_position = min(data)
-    crabs = dict(Counter(data))
-
-    print('Part 1: ', get_best_fuel_consumption(is_part1=True))
-    print('Part 2: ', get_best_fuel_consumption(is_part1=False))
+    print('Part 1: ', part_1())
+    print('Part 2: ', part_2())
